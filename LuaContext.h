9d46ae9b9b94e425574189566831305cafbf2cdc
Ugo <uv@cyberhipster.net>
2012-01-11 19:07:21 +0100

    Fixed doxygen configuration

--- LuaContext.h
+++ LuaContext.h
@@ -23,6 +23,10 @@
 // ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+///@file
+/// Lua context: Create new or wrap existing Lua state. 
+
 extern "C" {
 #include "lua.h"
 #include "lauxlib.h"
