a847ba3444fb1a94d79701806bbe2a51054f6ed2
candycode <ugovaretto@gmail.com>
2012-12-11 16:57:51 +0100

    Updated license

--- LuaArguments.h
+++ LuaArguments.h
@@ -1,5 +1,5 @@
 #pragma once
-// QLua - Copyright (c) 2012, Ugo Varetto
+//QLua - Copyright (c) 2012, Ugo Varetto
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
@@ -9,8 +9,8 @@
 //     * Redistributions in binary form must reproduce the above copyright
 //       notice, this list of conditions and the following disclaimer in the
 //       documentation and/or other materials provided with the distribution.
-//     * Neither the name of the <organization> nor the
-//       names of its contributors may be used to endorse or promote products
+//     * Neither the name of the author and copyright holder nor the
+//       names of contributors to the project may be used to endorse or promote products
 //       derived from this software without specific prior written permission.
 //
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
